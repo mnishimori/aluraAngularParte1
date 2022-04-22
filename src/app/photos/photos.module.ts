4f569767dbdc05ae33ffe63d961d53c2185b677f
mnishimori <mnishimori@yahoo.com.br>
2022-04-22 09:12:33 -0300

    Criação de novo componente, declaração de componente em um módulo, @Input(), criação de módulo e diretiva *ngFor

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,14 @@
+import { PhotoComponent } from './photo/photo.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [ PhotoComponent ],
+  imports: [
+    CommonModule
+  ],
+  exports: [ PhotoComponent ]
+})
+export class PhotosModule { }
