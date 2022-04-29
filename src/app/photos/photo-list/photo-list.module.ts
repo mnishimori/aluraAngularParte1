80373e355e24aa3b5cd51ed286c156f5d42b918c
mnishimori <mnishimori@yahoo.com.br>
2022-04-29 08:23:16 -0300

    Diretivas

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -8,6 +8,7 @@ import { PhotoListComponent } from './photo-list.component';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { PhotosComponent } from './photos/photos.component';
 import { SearchComponent } from './search/search.component';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { SearchComponent } from './search/search.component';
   imports: [
     CommonModule,
     PhotoModule,
-    CardModule
+    CardModule,
+    DarkenOnHoverModule
   ]
 })
 export class PhotoListModule { }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,3 +1,4 @@
+import { DarkenOnHoverModule } from './../shared/directives/darken-on-hover/darken-on-hover.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoModule } from './photo/photo.module';
