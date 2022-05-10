c7a615bc38d5a82d87421bf33dc86f8f522e598c
mnishimori <mnishimori@yahoo.com.br>
2022-05-10 08:46:37 -0300

    Footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,16 +5,22 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [HeaderComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
