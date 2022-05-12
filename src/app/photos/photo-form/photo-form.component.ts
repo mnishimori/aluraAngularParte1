7241306fd0519c51cf01c80797117ee8bd22469a
mnishimori <mnishimori@yahoo.com.br>
2022-05-12 08:06:37 -0300

    Upload de fotos

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
+import { PhotoService } from './../photo/photo.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-photo-form',
@@ -11,7 +13,11 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.photoForm = this.formBuilder.group({
@@ -24,9 +30,9 @@ export class PhotoFormComponent implements OnInit {
   upload(): void {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
-    console.log(description);
-    console.log(allowComments);
-    console.log(this.file);
+
+    this.photoService.upload(description, allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']));
   }
 
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 
 import { Photo } from './photo';
 
+const API = 'http://localhost:3000'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,14 +16,23 @@ export class PhotoService {
   }
 
   listFromUser(userName: string): Observable<Photo[]>{
-    return this.http.get<Photo[]>(`http://localhost:3000/${userName}/photos`);
+    return this.http.get<Photo[]>(`${API}/${userName}/photos`);
   }
 
   listFromUserPaginated(userName: string, page: number): Observable<Photo[]>{
     const params = new HttpParams()
       .append('page', page.toString());
 
-    return this.http.get<Photo[]>(`http://localhost:3000/${userName}/photos`, { params });
+    return this.http.get<Photo[]>(`${API}/${userName}/photos`, { params });
+  }
+
+  upload(description: string, allowComments: boolean, file: File) {
+    const formData = new FormData();
+    formData.append('description', description);
+    formData.append('allowComments', allowComments ? 'true' : 'false');
+    formData.append('imageFile', file);
+
+    return this.http.post(`${API}/photos/upload`, formData);
   }
 
 }
