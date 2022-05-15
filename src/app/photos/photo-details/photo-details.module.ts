0cee2f58a83c21efbbebf81ad66f8c668073149e
mnishimori <mnishimori@yahoo.com.br>
2022-05-15 11:46:11 -0300

    Componente de exibição e inclusão de comentário

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -2,12 +2,14 @@ import { PhotoModule } from './../photo/photo.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoDetailsComponent } from './photo-details.component';
+import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
 
 
 
 @NgModule({
   declarations: [
-    PhotoDetailsComponent
+    PhotoDetailsComponent,
+    PhotoCommentsComponent
   ],
   imports: [
     CommonModule,
